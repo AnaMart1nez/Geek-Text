d672cc1b8e088665bb83b6b9a31531b944b460eb
Ana Martinez <61203063+AnaMart1nez@users.noreply.github.com>
2020-09-29 01:41:17 -0400

    GeekTextDB
    Modified attributes, relations, data types and keys

--- mysql/GeekTextDB.sql
+++ mysql/GeekTextDB.sql
@@ -1,44 +1,6 @@
 CREATE DATABASE GeekTextDB;
 USE GeekTextDB;
 
-/** CreditCard - Stores various credit cards 
-per user as payment method **/
-CREATE TABLE `CreditCards` (
-  `Credit CardNumber` varchar(19) not null,
-  `Username` varchar(25) not null,
-  `CardholderName` varchar(50) not null,
-  `ExpirationDate` date not null,
-  `CVV` varchar(4) not null,
-  PRIMARY KEY (`Credit CardNumber`),
-  KEY `PK, FK` (`Username`)
-);
-
-/** Genres - Lists possible book genres **/
-CREATE TABLE `Genres` (
-  `GenreID` int not null auto_increment,
-  `GenreName` varchar(50) not null,
-  PRIMARY KEY (`GenreID`)
-);
-
-/** ShoppingCart - Each user has their unique 
-shoping cart with the current balance **/
-CREATE TABLE `ShoppingCart` (
-  `ShoppingCartID` int not null auto_increment,
-  `Username` varchar(25) not null,
-  `SubTotal` decimal(12,2) not null,
-  PRIMARY KEY (`ShoppingCartID`)
-);
-
-/** Authors - Lists authors info **/
-CREATE TABLE `Authors` (
-  `AuthorID` int not null auto_increment,
-  `FirstName` varchar(15) not null,
-  `MiddleName` varchar(15) not null,
-  `LastName` varchar(20) not null,
-  `Biography` text null,
-  PRIMARY KEY (`AuthorID`)
-);
-
 /** Users - Lists users created and 
 their account info **/
 CREATE TABLE `Users` (
@@ -47,10 +9,50 @@ CREATE TABLE `Users` (
   `MiddleName` varchar(15) not null,
   `LastName` varchar(20) not null,
   `Email` varchar(50) not null,
-  `Password` varchar(20),
+  `Password` varchar(20) not null,
   `Nickname` varchar(25) not null,
-  `PrivacySettings` boolean default False,
+  `PrivacySettings` boolean default False not null,
   PRIMARY KEY (`Username`)
+);
+
+/** Home Address - Stores each user Home Address**/
+CREATE TABLE `Home Address` (
+ `Username` varchar(25) not null,
+  `Street` varchar(50) not null,
+  `Apt` varchar(5) null,
+  `City` varchar(25) not null,
+  `State` varchar(25) not null,
+  `ZipCode` varchar(10) not null,
+  `Country` varchar(25) not null,
+  PRIMARY KEY (`Username`),
+  FOREIGN KEY (`Username`) references GeekTextDB.Users(`Username`)
+);
+
+/** Shipping Address - Stores various Shipping 
+Addresses per user **/
+CREATE TABLE `ShippingAddress` (
+  `ShippingID` int not null auto_increment,
+  `Username` varchar(25) not null,
+  `Street` varchar(50) not null,
+  `Apt` varchar(5) null,
+  `City` varchar(25) not null,
+  `State` varchar(25) not null,
+  `ZipCode` varchar(10) not null,
+  `Country` varchar(25) not null,
+  PRIMARY KEY (`ShippingID`),
+  FOREIGN KEY (`Username`) REFERENCES GeekTextDB.Users(`Username`)
+);
+
+/** CreditCard - Stores various credit cards 
+per user as payment method **/
+CREATE TABLE `CreditCards` (
+  `CreditCardNumber` varchar(19) not null,
+  `Username` varchar(25) not null,
+  `CardholderName` varchar(50) not null,
+  `ExpirationDate` date not null,
+  `CVV` varchar(4) not null,
+  PRIMARY KEY (`CreditCardNumber`),
+  FOREIGN KEY (`Username`) REFERENCES GeekTextDB.Users(`Username`)
 );
 
 /** Publishers - Lists possible book publishers **/
@@ -60,12 +62,21 @@ CREATE TABLE `Publishers` (
   PRIMARY KEY (`PublisherID`)
 );
 
-/** WrittenBy - Links all Authors with the Books 
-they have written **/
-CREATE TABLE `WrittenBy` (
+/** Genres - Lists possible book genres **/
+CREATE TABLE `Genres` (
+  `GenreID` int not null auto_increment,
+  `GenreName` varchar(50) not null,
+  PRIMARY KEY (`GenreID`)
+);
+
+/** Authors - Lists authors info **/
+CREATE TABLE `Authors` (
   `AuthorID` int not null auto_increment,
-  `ISBN` varchar(13) not null,
-  KEY `PK, FK` (`AuthorID`, `ISBN`)
+  `FirstName` varchar(15) not null,
+  `MiddleName` varchar(15) not null,
+  `LastName` varchar(20) not null,
+  `Biography` varchar(500),
+  PRIMARY KEY (`AuthorID`)
 );
 
 /** Books - Lists all book details and records 
@@ -76,37 +87,96 @@ CREATE TABLE `Books` (
   `Cover` varchar(255) not null,
   `GenreID` int not null,
   `Price`  decimal(6,2) not null,
-  `DatePublished` date not null,
+  `DatePublished` varchar(10) not null,
   `PublisherID` int not null,
-  `Summary` text not null,
-  `BookSells` int null, 
-  PRIMARY KEY (`ISBN`)
+  `Summary` varchar(500) not null,
+  `BookSells` decimal(3,0) default 0 not null, 
+  PRIMARY KEY (`ISBN`),
+  FOREIGN KEY (`GenreID`) REFERENCES GeekTextDB.Genres(`GenreID`),
+  FOREIGN KEY (`PublisherID`) REFERENCES GeekTextDB.Publishers(`PublisherID`)
 );
 
-/** Shipping Address - Stores various Shipping 
-Addresses per user **/
-CREATE TABLE `Shipping Address` (
-  `Username` varchar(25) not null,
-  `ShippingID` int not null auto_increment,
-  `Street` varchar(50) not null,
-  `Apt` varchar(5) null,
-  `City` varchar(25) not null,
-  `State` varchar(25) not null,
-  `Zip Code` varchar(10) not null,
-  `Country` varchar(25) not null,
-  PRIMARY KEY (`ShippingID`),
-  KEY `PK, FK` (`Username`)
+/** WrittenBy - Links all Authors with the Books 
+they have written **/
+CREATE TABLE `WrittenBy` (
+  `AuthorID` int not null,
+  `ISBN` varchar(13) not null,
+  PRIMARY KEY (`ISBN`),
+  FOREIGN KEY (`AuthorID`) REFERENCES GeekTextDB.Authors(`AuthorID`),
+  FOREIGN KEY (`ISBN`) REFERENCES GeekTextDB.Books(`ISBN`)
 );
 
 /** WishList - Holds up to 3 Wishlists per user**/
 CREATE TABLE `WishList` (
   `WishListID` int not null auto_increment,
   `Username` varchar(25) not null,
-  `WishListIndex` decimal (1,0) not null default 0,
+  `WishListIndex` int not null,
   `WishListName` varchar(25) not null,
-  `DateCreated` timestamp not null,
-  PRIMARY KEY (`WishListID`, `WishListIndex`),
-  KEY `PK, FK` (`Username`)
+  PRIMARY KEY (`WishListID`),
+  FOREIGN KEY (`Username`) REFERENCES GeekTextDB.Users(`Username`)
+);
+
+
+
+
+/** WishListItems - Holds which items and the quantity 
+added to the user Wishlist **/
+CREATE TABLE `WishListItems` (
+  `WishListItemID` int not null auto_increment,
+  `WishListID` int not null,
+  `Username` varchar(25) not null,
+  `ISBN` varchar(13) not null,
+  PRIMARY KEY (`WishListItemID`),
+  FOREIGN KEY (`WishListID`) REFERENCES GeekTextDB.WishList(`WishListID`),
+  FOREIGN KEY (`Username`) REFERENCES GeekTextDB.Users(`Username`),
+  FOREIGN KEY (`ISBN`) REFERENCES GeekTextDB.Books(`ISBN`)
+);
+
+/** ShoppingCart - Each user has their unique 
+shoping cart with the current balance **/
+CREATE TABLE `ShoppingCart` (
+  `ShoppingCartID` int not null auto_increment,
+  `Username` varchar(25) not null,
+  `SubTotal` decimal(12,2) not null, 
+  PRIMARY KEY (`ShoppingCartID`),
+  FOREIGN KEY (`Username`) REFERENCES GeekTextDB.Users(`Username`)
+);
+
+/** ShoppingCartItems - Holds all the items on a Shopping Cart **/
+CREATE TABLE `ShoppingCartItems` (
+  `ShoppinCartItemID` int not null auto_increment,
+  `ShoppingCartID` int not null,
+  `ISBN` varchar(13) not null,
+  `Quantity` decimal(3,0) not null default 1,
+  `SaveForLater` boolean default false not null,
+  PRIMARY KEY (`ShoppinCartItemID`),
+  FOREIGN KEY (`ISBN`) references GeekTextDB.Books(`ISBN`),
+  FOREIGN KEY (`ShoppingCartID`) references GeekTextDB.ShoppingCart(`ShoppingCartID`)
+);
+
+/** Order - Lists all the details of an Order **/
+CREATE TABLE `Orders` (
+  `OrderID` int not null auto_increment,
+  `Username` varchar(25) not null,
+  `ShippingID` int not null,
+  `TotalPrice` decimal(12,2) not null,
+  `DateOrdered` timestamp not null,
+  `CreditCardNumber` varchar(19) not null,
+  PRIMARY KEY (`OrderID`),
+  FOREIGN KEY (`CreditCardNumber`) REFERENCES GeekTextDB.CreditCards(`CreditCardNumber`),
+  FOREIGN KEY (`ShippingID`) REFERENCES GeekTextDB.ShippingAddress(`ShippingID`),
+  FOREIGN KEY (`Username`) REFERENCES GeekTextDB.Users(`Username`)
+);
+
+/** Reviews - Holds all the items on an Order **/
+CREATE TABLE `OrderItems` (
+  `OrderItemID` int not null auto_increment,
+  `OrderID` int not null,
+  `ISBN` varchar(13) not null,
+  `Quantity` decimal(5,0) not null default 1,
+  PRIMARY KEY (`OrderItemID`),
+  FOREIGN KEY (`OrderID`) references GeekTextDB.Orders(`OrderID`),
+  FOREIGN KEY (`ISBN`) references GeekTextDB.Books(`ISBN`)
 );
 
 /** Reviews - Holds the reviews and ratings each user has 
@@ -115,59 +185,11 @@ CREATE TABLE `Reviews` (
   `Username` varchar(25) not null,
   `OrderID` int not null,
   `ISBN` varchar(13) not null,
-  `Rating` decimal (1,0) not null default 0,
-  `Comments` text null,
-  `DatePosted` timestamp,
-  KEY `PK, FK` (`Username`, `OrderID`, `ISBN`)
-);
-
-/** WishListItems - Holds which items and the quantity 
-added to the user Wishlist **/
-CREATE TABLE `WishListItems` (
-  `WishListItemID` int not null auto_increment,
-  `WishListID` int not null,
-  `ISBN` varchar(13) not null,
-  PRIMARY KEY (`WishListItemID`)
-);
-
-/** Order - Lists all the details of an Order **/
-CREATE TABLE `Order` (
-  `OrderID` int not null auto_increment,
-  `Username` varchar(25) not null,
-  `ShippingID` int not null,
-  `TotalPrice` decimal(12,2) not null,
-  `DateOrdered` timestamp not null,
-  `CreditCardNumber` varchar(19) not null,
-  PRIMARY KEY (`OrderID`)
-);
-
-/** ShoppingCartItems - Holds all the items on a Shopping Cart **/
-CREATE TABLE `ShoppingCartItems` (
-  `ShoppinCartItemID` int not null auto_increment,
-  `ShoppingCartID` int not null,
-  `ISBN` varchar(13) not null,
-  `Quantity` decimal(5,0),
-  `SaveForLater` boolean default false,
-  PRIMARY KEY (`ShoppinCartItemID`)
-);
-
-/** Reviews - Holds all the items on an Order **/
-CREATE TABLE `OrderItems` (
-  `OrderItemID` int not null auto_increment,
-  `OrderID` int not null,
-  `ISBN` varchar(13) not null,
-  `Quantity` decimal(5,0),
-  PRIMARY KEY (`OrderItemID`)
-);
-
-/** Home Address - Stores each user Home Address**/
-CREATE TABLE `Home Address` (
- `Username` varchar(25) not null,
-  `Street` varchar(50) not null,
-  `Apt` varchar(5) null,
-  `City` varchar(25) not null,
-  `State` varchar(25) not null,
-  `Zip Code` varchar(10) not null,
-  `Country` varchar(25) not null,
-  KEY `PK, FK` (`Username`)
+  `Rating` int not null,
+  `Comments` varchar(255) null,
+  `DatePosted` timestamp not null,
+  PRIMARY KEY(`Username`, `OrderID`, `ISBN`),
+  FOREIGN KEY (`Username`) REFERENCES GeekTextDB.Users(`Username`),
+  FOREIGN KEY (`OrderID`) REFERENCES GeekTextDB.Orders(`OrderID`),
+  FOREIGN KEY (`ISBN`) REFERENCES GeekTextDB.Books(`ISBN`)
 );
